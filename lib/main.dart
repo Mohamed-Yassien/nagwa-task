747293825a1db7de6d5908c556022fa37d1ef3fa
mali <mali@tafeel.com>
2025-04-24 12:23:18 +0200

    remove unsued package

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,15 @@ import 'package:hive/hive.dart';
 import 'package:nagwa/core/app_constants.dart';
 import 'package:nagwa/core/network/dio_helper.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'books/domain/entities/author_entity.dart';
 import 'books/domain/entities/book_entity.dart';
 import 'books/domain/entities/books_reponse_entity.dart';
 import 'core/services/service_locator.dart';
 import 'books/presentation/screens/books_screen.dart';
 
-late SharedPreferences prefs;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  prefs = await SharedPreferences.getInstance();
   DioHelper.init();
   ServiceLocator().init();
   final appDocDir = await getApplicationDocumentsDirectory();
